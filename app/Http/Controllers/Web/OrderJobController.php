9c0a7a75513d3a0a33dbc440b541a7ca6813551d
Erwin Rahayu <artmxarea@gmail.com>
2019-10-09 05:18:23 +0700

    null

--- app/Http/Controllers/Web/OrderJobController.php
+++ app/Http/Controllers/Web/OrderJobController.php
@@ -117,7 +117,7 @@ class OrderJobController extends Controller
         $result = $this->OrderJobRepo->getDetail($id);
         $data = collect($result);
 
-        // dd($result);
+         dd($data);
 
         $rightTopControls = ['document.getElementById("rightTopControl")'];
         $this->gmap->injectControlsInRightTop = $rightTopControls;
