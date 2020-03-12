b54c281dee5e810bf626f97205467d900d920848
Erwin Rahayu <artmxarea@gmail.com>
2020-03-12 17:13:41 +0700

    added

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -88,12 +88,7 @@
                             </div>
                         </form>
                     </div>
-                    <div class="m-login__account">
-                        <span class="m-login__account-msg">
-                            Don't have an account yet ?
-                        </span>&nbsp;&nbsp;
-                        <a href="javascript:;" id="m_login_signup" class="m-link m-link--light m-login__account-link">Sign Up</a>
-                    </div>
+                    
                 </div>
             </div>
         </div>
